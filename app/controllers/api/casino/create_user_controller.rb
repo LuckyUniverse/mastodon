93aa47132364b8dc2aa6addc2a1e62f5c1037e87
Barrett Breshears <barrettbreshears@gmail.com>
2019-02-13 02:02:37 -0700

    Update create_user_controller.rb

--- app/controllers/api/casino/create_user_controller.rb
+++ app/controllers/api/casino/create_user_controller.rb
@@ -1,12 +1,10 @@
 class Api::Casino::CreateUserController < Api::BaseController
 
   def index
-
-    
     @email = params[:email]
     @password = params[:password]
     @username = params[:username]
-    @user = User.new(email: @email, password: @email, account_attributes: { username: @username })
+    @user = User.new(email: @email, password: @email, account_attributes: { username: @username }, agreement: true, confirmed_at: Time.now.utc )
     if @user.save 
       render json: {"success":true}
     else
