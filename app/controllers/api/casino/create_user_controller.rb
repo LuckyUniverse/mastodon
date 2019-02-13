36106b1d1ccd6fb2a0044e495b79e83cc1e4994b
Barrett Breshears <barrettbreshears@gmail.com>
2019-02-13 02:32:04 -0700

    Update create_user_controller.rb

--- app/controllers/api/casino/create_user_controller.rb
+++ app/controllers/api/casino/create_user_controller.rb
@@ -5,20 +5,13 @@ class Api::Casino::CreateUserController < Api::BaseController
     @email = params[:email]
     @password = params[:password]
     @username = params[:username]
-    @account  = Account.new(username: @username)
-    @user  = User.new(email: @email, password: @email, account_attributes: { username: @username }, agreement: true, confirmed_at: Time.now.utc )
+    @user  = User.new(email: @email, password: @password, account_attributes: { username: @username }, agreement: true, confirmed_at: Time.now.utc )
 
-    if @account.save 
-      @account.suspended = false
-      @user.account      = account
 
-      if @user.save 
-        render json: {"success":true, "user", @user}
-      else
-        render json: {"success":false, "error": @user.errors.full_messages}
-      end
-    else 
-      render json: {"success":false, "error": @account.errors.full_messages}
+    if @user.save 
+      render json: {"success":true, "user", @user}
+    else
+      render json: {"success":false, "error": @user.errors.full_messages}
     end
     
   end
